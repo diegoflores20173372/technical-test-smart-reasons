4b00f999883bf65d116397dd0f99eb75aecac3d7
DiegoF <dflorescam@unsa.edu.pe>
2022-12-07 11:14:16 -0500

    Change Home

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,9 @@
 import electronicBillingRouter from '@/modules/electronic-billing/router'
 import { createRouter, createWebHistory } from 'vue-router'
 
-import Home from '../views/Home.vue'
 
 const routes = [
-    {
-        path: '',
-        name: 'Home',
-        component: Home
-    },
+
     {
         path: '/bills',
         ...electronicBillingRouter
